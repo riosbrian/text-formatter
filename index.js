2c5084d76981571976cd3edddb41a77498e86cfd
riosbrian <riosbrian.ar@gmail.com>
2023-05-23 13:13:21 +0200

    finished css, added javascript functionality

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-"use strict";
+'use strict';
+
+const textarea = document.querySelector('.textarea');
+
+const OPTION = {
+  lowercase: (text) => (textarea.value = text.toLowerCase()),
+  uppercase: (text) => (textarea.value = text.toUpperCase()),
+  capitalize: (text) => {
+    const cap = text
+      .split(' ')
+      .map((word) => word.replace(word[0], word[0].toUpperCase()))
+      .join(' ');
+    textarea.value = cap;
+  },
+};
+
+const formatter = function ({ check, text }) {
+  OPTION[check](text);
+};
+
+const form = document.querySelector('.formatterForm');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(e.target));
+  formatter(data);
+});
